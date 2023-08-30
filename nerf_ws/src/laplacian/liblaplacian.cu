8a83453d3521db42d57a26cfb4e37e379441e687
Paul Gesel <paulgesel@gmail.com>
2023-08-29 22:34:20 -0400

    use softmax instead of laplacian

--- nerf_ws/src/laplacian/liblaplacian.cu
+++ nerf_ws/src/laplacian/liblaplacian.cu
@@ -5,29 +5,80 @@
 namespace internal {
     constexpr int blockSize = 256;
 
-    __global__ void kernel(float *vec, int x_dims, int num_elements) {
+    __global__ void kernel(float *vec, int x_dims, int y_dims) {
         __shared__ float s[4 * blockSize];
-        int idx = 2 * (blockIdx.x * blockDim.x + threadIdx.x);
-        const int left_idx = idx - 2;
-        const int up_idx = idx - 2 * x_dims;
-        const int right_idx = idx + 2;
-        const int down_idx = idx + 2 * x_dims;
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        int ind_x = idx % x_dims;
+        int ind_y = idx / x_dims;
+        constexpr int stride = 3;
 
-        if (idx < num_elements && left_idx % (2*x_dims) > 0 && up_idx > 0  && down_idx < num_elements && vec[idx + 1] < 1.0) {
+        if (ind_x > 0 && ind_x < x_dims - 1 && ind_y > 0 && ind_y < y_dims - 1) {
 
-            //TODO use shared mem
+            const int left_idx = stride * idx - stride;
+            const int up_idx = stride * idx - stride * x_dims;
+            const int right_idx = stride * idx + stride;
+            const int down_idx = stride * idx + stride * x_dims;
+
+            const int up_left_idx = up_idx - stride;
+            const int up_right_idx = up_idx + stride;
+            const int down_left_idx = down_idx - stride;
+            const int down_right_idx = down_idx + stride;
+
             const unsigned int shared_idx = 4 * (threadIdx.x % blockSize);
             s[shared_idx] = vec[up_idx];
             s[shared_idx + 1] = vec[left_idx];
             s[shared_idx + 2] = vec[right_idx];
             s[shared_idx + 3] = vec[down_idx];
+
+//            float val = min(min(vec[up_idx], vec[down_idx]), min(vec[left_idx], vec[right_idx])) + .1f;
+//            float val_diag =
+//                    min(min(vec[up_left_idx], vec[up_right_idx]), min(vec[down_left_idx], vec[down_right_idx])) + .141f;
+//            val = min(val, val_diag);
+
+            float val = vec[up_idx] * exp(-vec[up_idx]) + vec[down_idx] * exp(-vec[down_idx]) +
+                        vec[left_idx] * exp(-vec[left_idx]) + vec[right_idx] * exp(-vec[right_idx]) +
+                        vec[up_left_idx] * exp(-vec[up_left_idx]) + vec[up_right_idx] * exp(-vec[up_right_idx]) +
+                        vec[down_left_idx] * exp(-vec[down_left_idx]) + vec[down_right_idx] * exp(-vec[down_right_idx]);
+            val = val / (exp(-vec[up_idx]) + exp(-vec[down_idx]) + exp(-vec[left_idx]) + exp(-vec[right_idx]) +
+                         exp(-vec[up_left_idx]) + exp(-vec[up_right_idx]) + exp(-vec[down_left_idx]) +
+                         exp(-vec[down_right_idx]));
+            val += .005;
+
             __syncthreads();
-            vec[idx] = 0.25 * (s[shared_idx] + s[shared_idx + 1] + s[shared_idx + 2] + s[shared_idx + 3]);
-
-//            const float val =  .25*(vec[up_idx]+vec[left_idx]+vec[right_idx]+vec[down_idx]);
-//            __syncthreads();
-//            vec[idx] = val;
-
+//            4*u(x,y) = u(x-1,y)+u(x+1,y)+u(x,y+1)+u(x,y-1);
+//            0 = u(x-1,y)+u(x+1,y)+u(x,y+1)+u(x,y-1) - 4*u(x,y);
+//            0 = nabla u(x,y)
+            if (vec[stride * idx + 1] < 0.5) {
+                // no constraint
+//                vec[stride * idx] = 0.25 * (s[shared_idx] + s[shared_idx + 1] + s[shared_idx + 2] + s[shared_idx + 3]);
+                vec[stride * idx] = val;
+            } else if (vec[stride * idx + 1] < 1.5) {
+                // temperature constraints
+                vec[stride * idx] = vec[stride * idx + 2];
+            } else if (vec[stride * idx + 1] < 2.5) {
+                // flux x constraints
+                // T[n]-T[n-1]/dx = q
+                // T[n+1]-T[n-1]/2dx = q
+                float &flux = vec[stride * idx + 2];
+                if (ind_x > 1) {
+                    vec[stride * idx] = s[shared_idx + 1] + flux;
+                } else {
+                    vec[stride * idx] = s[shared_idx + 2] + flux;
+                }
+            } else if (vec[stride * idx + 1] < 3.5) {
+                // flux y constraints
+                float &flux = vec[stride * idx + 2];
+                if (ind_y > 1) {
+                    vec[stride * idx] = s[shared_idx] + flux;
+                } else {
+                    vec[stride * idx] = s[shared_idx + 3] + flux;
+                }
+            } else if (vec[stride * idx + 1] < 4.5) {
+                // source/sink constraints
+                float &flux = vec[stride * idx + 2];
+                vec[stride * idx] =
+                        0.25 * (s[shared_idx] + s[shared_idx + 1] + s[shared_idx + 2] + s[shared_idx + 3] + flux);
+            }
         }
     }
 
@@ -36,12 +87,7 @@ namespace internal {
 
         int numThreads = num_elements;
         int gridSize = (numThreads + blockSize - 1) / blockSize;
-        kernel<<<gridSize, blockSize>>>(vec, x_dims, 2 * num_elements);
-
-//        dim3 dimBlock(256, 1, 1);
-//        dim3 dimGrid(ceil((float) num_elements / dimBlock.x));
-//        kernel<<<dimGrid, dimBlock>>>(vec, num_elements);
-
+        kernel<<<gridSize, blockSize>>>(vec, x_dims, y_dims);
 
         cudaError_t error = cudaGetLastError();
         if (error != cudaSuccess) {
