ed14a40eb0f9c7482fdfeb72ebdde8f3d7998d66
Paul Gesel <paulgesel@gmail.com>
2023-11-03 14:05:36 -0400

    add notebook to run policy

--- scripts/operational_velocity.py
+++ scripts/operational_velocity.py
@@ -1,7 +1,8 @@
 from robot_library_py import *
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Joy, JointState
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float32MultiArray
 from intera_core_msgs.msg import JointCommand
 
 from threading import Thread
@@ -11,14 +12,14 @@ import numpy as np
 
 class ROSInterface:
     def __init__(self, node, robot):
-        self.joy_msg = None
+        self.vel_msg = None
         self.joint_states_msg = None
         self.robot = robot
         self.node = node
         self.jointMap = {name: ind for ind, name in enumerate(self.robot.jointNames)}
 
-    def joy_callback(self, msg):
-        self.joy_msg = msg
+    def vel_callback(self, msg):
+        self.vel_msg = msg
 
     def joint_states_callback(self, msg):
         self.joint_states_msg = msg
@@ -30,7 +31,7 @@ class ROSInterface:
 
     def spin_thread(self):
         self.node.create_subscription(JointState, 'robot/joint_states', self.joint_states_callback, 10)
-        self.node.create_subscription(Joy, '/joy', self.joy_callback, 10)
+        self.node.create_subscription(Float32MultiArray, '/operational_velocity', self.vel_callback, 10)
         rclpy.spin(self.node)
 
 
@@ -47,7 +48,7 @@ yaw = lambda msg: (msg.axes[2] / 1) * (1 - mode(msg))
 def run():
     rclpy.init()
     robot = URDFModel('sawyer.urdf')
-    node = Node('joy_node')
+    node = Node('vel_node')
     q = robot.getJoints()
     ros_interface = ROSInterface(node, robot)
 
@@ -66,24 +67,14 @@ def run():
     gripper_msg.position = [0.0, 0.0]
     last_trigger = 0
     while rclpy.ok():
-        if ros_interface.joy_msg and ros_interface.joint_states_msg:
-            print(ros_interface.joy_msg)
-            m = ros_interface.joy_msg
-            xd = np.array([forward(m), right(m), up(m), roll(m), pitch(m), yaw(m)])
-            J = robot.getJacobian('camera_link')
+        if ros_interface.vel_msg and ros_interface.joint_states_msg:
+            print(ros_interface.vel_msg)
+            xd = np.array(ros_interface.vel_msg)
+            J = robot.getJacobian('right_gripper_l_finger')
             qd = np.linalg.inv(J.T @ J + alpha * I) @ J.T @ xd
             cmd_msg.velocity = list(qd)
 
             cmd_pub.publish(cmd_msg)
-            if trigger(ros_interface.joy_msg) == 1 and last_trigger == 0:
-                if gripper_msg.position[0] == 0.0:
-                    gripper_msg.position = [.03, -.03]
-                else:
-                    gripper_msg.position = [0.0, 0.0]
-                cmd_pub.publish(gripper_msg)
-                last_trigger = 1
-            elif trigger(ros_interface.joy_msg) == 0:
-                last_trigger = 0
             time.sleep(.1)
 
 
